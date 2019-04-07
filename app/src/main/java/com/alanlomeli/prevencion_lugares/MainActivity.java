45f80c91e966d7eed60120ae3c0a5c1f2bb71374
Mario00375 <49355365+Mario00375@users.noreply.github.com>
2019-04-06 22:50:09 -0600

    puse una nota

--- app/src/main/java/com/alanlomeli/prevencion_lugares/MainActivity.java
+++ app/src/main/java/com/alanlomeli/prevencion_lugares/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    //niño agregue una nota
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
